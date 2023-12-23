0aef5b96e74d8e3e2c3f079921e59fd3918967c1
Aayush Bisht <97892535+aayushbisht@users.noreply.github.com>
2023-12-23 21:23:40 +0530

    Update CompanyList.jsx

--- frontend/src/components/CompanyList.jsx
+++ frontend/src/components/CompanyList.jsx
@@ -30,7 +30,7 @@ const CompanyList = () => {
 
   useEffect(() => {
     getCompanyData();
-    const newSocket = io();
+    const newSocket = io.connect("https://yoga-backend-4i5c.onrender.com");
     setSocket(newSocket);
 
     return () => {
